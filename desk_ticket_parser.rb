32478b149d1471aa0d3cfc2f58a1dc9dc0c66a45
mikrob <mikael.robert@clicrdv.com>
2017-06-22 16:26:40 +0200

    work script

---
+++ desk_ticket_parser.rb
@@ -0,0 +1,23 @@
+date = Date.new(2017,01,01)
+
+
+search_url = "https://clicrdv.desk.com/api/v2/cases/search?subject=SMS&since_created_date=#{date_to_timestamp(date)}&fields=id,created_at,subject,blurb"
+
+# search_url = "https://clicrdv.desk.com/api/v2/cases/search?labels=bug&subject=sms&since_created_at=#{date_to_timestamp(date)}&fields=id,created_at,subject,blurb"
+parsed_url = Addressable::URI.parse(search_url).normalize.to_str
+
+username = ENV["DESK_USERNAME"]
+password = ENV["DESK_PASSWORD"]
+
+response = RestClient::Request.execute method: :get, url: parsed_url, user: username, password: password
+tickets =  JSON.parse(response.body)
+
+tickets_array = tickets["_embedded"]["entries"]
+number = tickets["total_entries"]
+
+pp "We have #{number} tickets matching"
+
+#  tickets_per_month = tickets["cases"].group_by { |record| record["created_at"].to_date.strftime("%m-%y") }
+#  tickets_per_month.each do |month_year, tickets|
+#    puts "#{month_year} : #{tickets.count}"
+#  end
